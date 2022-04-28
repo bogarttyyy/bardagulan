bc254216f4237b9dd3090f5ed2f8ea7a16297410
Den Abad <dennis.abad6@gmail.com>
2022-04-28 23:36:43 +0800

    Fixed null for touch indicator in level manager

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -7,16 +7,11 @@ public class LevelManager : MonoBehaviour
     [SerializeField]
     private float timer = 10f;
 
-    [SerializeField]
-    private GameObject touchIndicator;
-
     public TrashSpawner spawner;
 
     // Update is called once per frame
     void Update()
     {
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        touchIndicator.transform.position = mousePos;
+        
     }
 }
